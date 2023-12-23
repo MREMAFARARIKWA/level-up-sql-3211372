db06681df5c305ee99ece2cb3de9bbd5fd0d3f08
MREMAFARARIKWA <154620191+MREMAFARARIKWA@users.noreply.github.com>
2023-12-23 09:06:10 +0200

    Update 01_01_invitations_ch.sql

--- 01_restaurant/challenges/01_01_invitations_ch.sql
+++ 01_restaurant/challenges/01_01_invitations_ch.sql
@@ -1,4 +1,6 @@
 -- Generate a list of customer information.
-
+select (first.name, last.name, email.adress
+  from customer
+  orderby last.name 
 -- Show their first name, last name, and email address.
 -- Sort the list of results by last name.
